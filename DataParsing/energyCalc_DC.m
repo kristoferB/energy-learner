9f32943d3510aa42ed9dbb766ca967f6ecade73d
David Sondell <dsondell@gmail.com>
2017-11-14 13:55:14 +0100

    Ignoring subfolder RAW but not formated data

--- DataParsing/energyCalc_DC.m
+++ DataParsing/energyCalc_DC.m
@@ -1,7 +1,7 @@
 clear variables
 clc, close all
 
-rawData = importdata('160429 - E - R30_original.csv');
+rawData = importdata('Data\Original\R30\E-R30_original.csv');
 I = rawData.data(:,2);
 V = rawData.data(:,3);
 
@@ -23,7 +23,6 @@ V = rawData.data(ns:nf,3);
 
 %% Energy and power, including cabinet and regeneration
 total_power = V.*I - P_cabinet; %Watt
-
 total_energy = sum(total_power)/sampleFrequency / 1000; %kW.s (kJouls)
 
 %energy_noCabinet = total_energy; %- (P_cabinet)*(nf-ns)*0.0001 / 1000;%  rms(P_cabinet)*(ns*0.0001)/ 1000 
@@ -34,22 +33,34 @@ consumedPower = (total_power>0).*(total_power);
 
 regenE = sum(regenPower)*0.0001 / 1000; %kW.s (kJouls)
 consumedE = sum(consumedPower)*0.0001 / 1000; %kW.s (kJouls)
-%% Peak power
 
+%% Peak power
 %peakPower = (max(consumedPower)- P_cabinet)/1000 ; %kWatt
 peakPower = max(consumedPower)/1000 ; %kWatt
+
 %%
-figure(2)
+figure(2), hold on
 plot(total_power)
+plot(consumedPower)
 %% Filter and downsample active power
 
-%close all
+r30_sampling = 0.012; %12ms
+
+active_power = downsample(total_power, r30_sampling*sampleFrequency);
+used_power = downsample(consumedPower, r30_sampling*sampleFrequency);
+
+
 figure(3)
-active_power = downsample(total_power,0.012*10000);
 plot(active_power,'b')
 hold on
 
 % ---- TEST DIFFERENT FILTERS
+% n = 100;
+% Wn = 0.1; % Frequency cutoff
+% b = fir1(n,Wn);
+% filt_power = conv(active_power,b);
+% % plot(filt_power,'black')
+% 
 % n = 24;
 % Wn = 0.1; % Frequency cutoff
 % b = fir1(n,Wn);
@@ -66,17 +77,29 @@ n = 12;
 Wn = 0.2; % Frequency cutoff
 b = fir1(n,Wn);
 filt_power = conv(active_power,b);
-% plot(filt_power,'green')
-% -----
+filt_usedPower = conv(used_power,b);
 
-% filt_power = conv(active_power,b);
+filt_power = filt_power(1:end-100);
+filt_usedPower = filt_usedPower(1:end-100);
+
+figure(3)
 plot(filt_power,'r')
 
 % -- Display Filter --
-%figure(4)
-%freqz(b,1,512)
+% figure(4)
+% freqz(b,1,512)
+
+%% READ TXT trajectorieis
+
+filename = 'Data\Original\R30\orig_traj.txt';
+delimiterIn = ' ';
+headerlinesIn = 5;
+A = importdata(filename,delimiterIn,headerlinesIn);
+A.data = A.data(1:end-5 , :);
+
 %%
 
-save('eOrig_R30.mat','total_energy','energy_noCabinet','consumedE','regenE','cycleTime','peakPower','filt_power');
-temp = [filt_power*2 filt_power];
-csvwrite('test.csv',temp);
+% save('eOrig_R30.mat','total_energy','energy_noCabinet','consumedE','regenE','cycleTime','peakPower','filt_power');
+headers = {'Time','Axis 1','Axis 2','Axis 3','Axis 4','Axis 5','Axis 6','Power [Watt]','Consumed Power [Watt]'};
+toCSV = [A.data filt_power filt_usedPower];
+csvwrite_with_headers('fitted_data.csv',toCSV,headers);
