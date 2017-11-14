206d0249f96461c4e1ac15d58bfba9381a2fa7c8
Viktor <viktor.eklind@gmail.com>
2017-11-14 09:51:39 +0100

    File structure

--- DataParsing/dataParsing.py
+++ DataParsing/dataParsing.py
@@ -6,5 +6,5 @@ energy_dataset = pd.read_csv('Data\Original\R30\E-R30_original.csv', sep=',', he
 
 traj_dataset= pd.read_table('Data\Original\R30\orig_traj.txt', sep=' ', header=None,
                             names=['Time', 'Axis 1', 'Axis 2', 'Axis 3', 'Axis 4', 'Axis 5', 'Axis 6'],
-                            skipinitialspace=True, skiprows=4, skipfooter=1)
+                            skipinitialspace=True, skiprows=4,skipfooter=1)
 print(traj_dataset)
