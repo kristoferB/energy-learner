10344070b9c51b3d53ac22483acf70b4862fc60d
Kim <kisv701@gmail.com>
2017-11-28 10:06:37 +0100

    Added a class for holding sampled data in a standardized way

---
+++ Trainers/SampledDataSet.py
@@ -0,0 +1,37 @@
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+class SampledDataSet:
+    """
+        Use this data holder class to hold sampled data, parsed with a parsing script similar to the parse_KR40 script
+        or provide another load_function. The loadfunction should return a pandas DataFrame with the following structure
+        Timestamps | Angle 1 | Angle 2 | Angle 3 ... | Angle 6 | Power
+          XX       |    XX   |   XX    |    XX   ... |   XX    |   XX
+        And so on...
+
+    """
+    def __init__(self, load_function, file_number):
+        data = load_function(file_number)
+        # Pick timestamps from loaded data
+        self.timestamps = data.values[:, 0:1]
+        # Pick measured angles from loaded data
+        self.angles = data.values[:, 1:-1]
+        # Pick power consumption from loaded data
+        self.power = data.values[:, -1:]
+        # pseudo power = velocity * acceleration
+        self.omega = np.gradient(self.angles, axis=0)
+        self.acc = np.gradient(self.omega, axis=0)
+        self.pseudo_power = np.multiply(self.omega, self.acc)
+        all_values = np.append(self.angles, self.omega, axis=1)  # Angles 1-6, Omega 1-6
+        all_values = np.append(all_values, self.acc, axis=1)
+        all_values = np.append(all_values, self.pseudo_power, axis=1)
+        # ensure that all data is float and rename to features to match with machine learning conventions
+        self.features = all_values.astype('float32')
+
+        # normalize features
+        self.scaler_features = MinMaxScaler(feature_range=(0, 1))
+        self.scaler_power = MinMaxScaler(feature_range=(0, 1))
+        self.scaled_features = self.scaler_features.fit_transform(self.features)
+        self.scaled_output = self.scaler_power.fit_transform(self.power)
+
+    def __str__(self):
+        return str(self.features)
--- Trainers/simple_prediction_training_seperate_files.py
+++ Trainers/simple_prediction_training_seperate_files.py
@@ -1,10 +1,12 @@
+from math import sqrt
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
 import numpy as np
 from matplotlib import pyplot
 from keras.models import Sequential
 from keras.layers import Dense
-
+from Trainers.SampledDataSet import SampledDataSet
 """
 This is the same simple network as used in the abs acceleration example but builds on training from different files.
 """
@@ -27,59 +29,43 @@ def plot_matrix(values):
     pyplot.show()
 
 
-class SampledDataSet:
-    """
-        Use this data holder class to hold sampled data, parsed with a parsing script similar to the parse_KR40 script
-        or provide another load_function. The loadfunction should return a pandas DataFrame with the following structure
-        Timestamps | Angle 1 | Angle 2 | Angle 3 ... | Angle 6 | Power
-          XX       |    XX   |   XX    |    XX   ... |   XX    |   XX
-        And so on...
 
-    """
-    def __init__(self,load_function, file_number):
-        data = load_function(file_number)
-        # Pick timestamps from loaded data
-        self.timestamps = data.values[:, 0:1]
-        # Pick measured angles from loaded data
-        self.angles = data.values[:, 1:-1]
-        # Pick power consumption from loaded data
-        self.power = data.values[:, -1:]
-        # pseudo power = velocity * acceleration
-        self.omega = np.gradient(self.angles, axis=0)
-        self.acc = np.gradient(self.omega, axis=0)
-        self.pseudo_power = np.multiply(self.omega, self.acc)
-        # ensure that all data is float and rename to features to match with machine learning conventions
-        self.features = self.pseudo_power.astype('float32')
-
-        # normalize features
-        self.scaler_features = MinMaxScaler(feature_range=(0, 1))
-        self.scaler_power = MinMaxScaler(feature_range=(0, 1))
-        self.scaled_features = self.scaler_features.fit_transform(self.features)
-        self.scaled_output = self.scaler_power.fit_transform(self.power)
-
-    def __str__(self):
-        return str(self.features)
 
 
 training_data = []
 for i in range(1, 10):
     training_data.append(SampledDataSet(load_160406, i))
 
+(_, input_dimension) = training_data[0].features.shape
+
 # design network
 model = Sequential()
-model.add(Dense(6, input_dim=6, activation="relu"))
+model.add(Dense(input_dimension, input_dim=input_dimension, activation="relu"))
+model.add(Dense(24, activation="relu"))
+model.add(Dense(24, activation="relu"))
 model.add(Dense(12, activation="relu"))
-model.add(Dense(12, activation="relu"))
-model.add(Dense(12, activation="relu"))
+model.add(Dense(6, activation="relu"))
 model.add(Dense(1, activation="sigmoid"))
 
 model.compile(optimizer="adam", loss="mean_squared_error")
-for i in range(0, 7):
-    history = model.fit(training_data[i].scaled_features, training_data[i].scaled_output, batch_size=26, epochs=60,
-                        validation_data=(training_data[8].scaled_features, training_data[8].scaled_output), verbose=2, shuffle=False)
+for i in range(8):
+    history = model.fit(training_data[i].scaled_features, training_data[i].scaled_output, batch_size=26, epochs=160,
+                        validation_data=(training_data[8].scaled_features, training_data[8].scaled_output), verbose=2,
+                        shuffle=False)
+    print('Fitting finished for index %f' % i)
 
-y_hat = model.predict(training_data[7].scaled_features)
-y_inv = training_data[7].scaler_power.inverse_transform(y_hat)
-pyplot.plot(y_inv)
-pyplot.plot(training_data[7].power)
+y_hat = model.predict(training_data[8].scaled_features)
+y_inv = training_data[8].scaler_power.inverse_transform(y_hat)
+plt_pred, = pyplot.plot(training_data[8].timestamps, y_inv, label='Prediction')
+plt_meas, = pyplot.plot(training_data[8].timestamps, training_data[8].power, label='Measurement 8')
+
+# calculate RMSE
+rmse = sqrt(mean_squared_error(y_inv, training_data[8].power))
+print('Test RMSE: %.3f' % rmse)
+rmse = sqrt(mean_squared_error(training_data[0].power, training_data[8].power))
+print('Seq diff between run 1 and 8 RMSE: %.3f' % rmse)
+
+pyplot.legend(handles=[plt_pred, plt_meas])
+pyplot.ylabel("Power [Watt]")
+pyplot.xlabel("Time [s]")
 pyplot.show()
